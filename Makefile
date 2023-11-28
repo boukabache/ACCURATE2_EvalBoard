78527a3e25b26060c2d1e982d2937537e00cc98b
Håkon Liverud <haakon.liverud@cern.ch>
2023-11-28 09:30:23 +0100

    Starting to implement i2c slave

--- Makefile
+++ Makefile
@@ -1,20 +1,14 @@
 VHDL_FILES := $(wildcard *.vhdl)
-JSON_FILE := final.json
+JSON_FILE := hardware.json
 ASC_FILE := $(JSON_FILE:.json=.asc)
 BIN_FILE := $(ASC_FILE:.asc=.bin)
-
-# Specify the top module for the top.vhdl
-TOP_MODULE := top
+O_FILE := $(VHDL_FILES:.vhdl=.o)
 
 all: $(BIN_FILE)
 
 $(JSON_FILE): $(VHDL_FILES)
 	@echo "Converting GHDL to JSON"
-	@if [ "$(VHDL_FILES)" = "$(TOP_MODULE).vhdl" ]; then \
-		yosys -m ghdl -p 'ghdl $(TOP_MODULE).vhdl -e top; write_json $@'; \
-	else \
-		yosys -m ghdl -p 'ghdl $(VHDL_FILES) -e; write_json $@'; \
-	fi
+	@yosys -m ghdl -p 'ghdl $(VHDL_FILES) -e $(TOP_MODULE); synth_ice40 -json $(JSON_FILE)'
 
 $(ASC_FILE): $(JSON_FILE)
 	@echo "Run place and route for $<"
@@ -24,11 +18,11 @@ $(BIN_FILE): $(ASC_FILE)
 	@echo "Packing to bin for $<"
 	@icepack $< $@
 	@echo "Removing intermediate files"
-	@rm -f $< $(<:.asc=.json)
+	#@rm -f $< $(<:.asc=.json)
 
 prog:
-	@iceprog final.bin
+	@iceprog hardware.bin
 
 clean:
 	@echo "Removing intermediate files"
-	@rm -f $(JSON_FILE) $(ASC_FILE) $(BIN_FILE)
+	@rm -f $(JSON_FILE) $(ASC_FILE) $(BIN_FILE) $(O_FILE)
--- top.vhdl
+++ top.vhdl
@@ -2,16 +2,100 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity top is
-    port (
-        led_green : out std_logic;
-        led_red : out std_logic;
-        led_blue : out std_logic
-    );
+  port (
+    led_green : out std_logic;
+    led_red : out std_logic;
+    led_blue : out std_logic;
+    scl : inout std_logic;
+    sda : inout std_logic;
+    sdaFeedback : out std_logic;
+    clk : in std_logic;
+    sw1 : in std_logic
+  );
 end entity top;
 
 architecture arch of top is
+  signal clk_100kHz : std_logic := '0';
+  signal counter : integer range 0 to 59 := 0;
+  signal second_counter : integer range 0 to 12000000 := 0;
+  signal send_message : std_logic := '0';
+
+  component I2C_slave is
+    generic (
+      SLAVE_ADDR : std_logic_vector(6 downto 0)
+    );
+    port (
+      scl : inout std_logic;
+      sda : inout std_logic;
+      clk : in std_logic;
+      read_req : out std_logic;
+      data_to_master : in std_logic_vector(7 downto 0);
+      data_valid : out std_logic;
+      data_from_master : out std_logic_vector(7 downto 0);
+      led_blue : out std_logic;
+      rst : in std_logic
+    );
+  end component I2C_slave;
+
+  signal data_valid : std_logic;
+  signal read_req : std_logic;
+  signal data_from_master : std_logic_vector(7 downto 0);
+  signal data_to_master : std_logic_vector(7 downto 0);
+  signal data_received : std_logic;
+  signal reset : std_logic;
+  signal bit_counter : integer range 0 to 7 := 0;  -- Add this line
+
+
+
 begin
-    led_green <= '0';
-    led_red <= '0';
-    led_blue <= '0';
+  led_red <= '1';
+
+  clk_gen : process (clk)
+  begin
+    if rising_edge(clk) then
+      if counter = 59 then
+        clk_100kHz <= not clk_100kHz;
+        counter <= 0;
+      else
+        counter <= counter + 1;
+      end if;
+
+      if second_counter = 12000000 then
+        send_message <= '1';
+        second_counter <= 0;
+      else
+        send_message <= '0';
+        second_counter <= second_counter + 1;
+      end if;
+    end if;
+  end process clk_gen;
+
+  reset_process : process (sw1)
+  begin
+    reset <= not sw1;
+  end process reset_process;
+
+
+
+  I2C_inst : I2C_slave
+  generic map (
+    SLAVE_ADDR => "0001111"
+  )
+  port map (
+    scl => scl,
+    sda => sda,
+    clk => clk_100kHz,
+    read_req => read_req,
+    data_to_master => data_to_master,
+    data_valid => data_valid,
+    data_from_master => data_from_master,
+    led_blue => led_blue,
+    rst => reset
+    );
+
+  sda <= sda;
+  scl <= scl;
+  sdaFeedback <= scl;
+
+  led_green <= data_received;
 end architecture arch;
