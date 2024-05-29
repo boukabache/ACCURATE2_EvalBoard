8a9d336aa626f24cd26cdb7382ad22100370327e
Mattia Consani <mattia.consani@cern.ch>
2024-05-29 12:23:00 +0200

    Add RX logic

--- hdl/UartLogic.vhd
+++ hdl/UartLogic.vhd
@@ -1,10 +1,16 @@
 --! @file UartLogic.vhd
 --! @brief UART logic module, the actual protocol is handled by the uart.vhd module
 --
---! For now, is only in charge of sending the voltageChangeIntervalxDI data 
---! out of the UART port.
---! Next steps are to also implement the reception of the data from the UART port
---! so that default values of DAC and ACCURATE can be set from the PC. 
+--! This module is instantiating the UartWrapper module and is responsible for
+--! the data transmission and reception logic.
+--
+--! TX: The data to be sent is sampled when the data ready signal is high.
+--! Then the data is sent byte by byte, using a sliding window from left to right.
+--
+--! RX: The data received is sampled when the FIFO is not empty. The received data
+--! is then assembled in 32 bits words and forwarded to the RegisterFile module.
+
+
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -46,25 +52,27 @@ architecture rtl of UartLogic is
     signal fifoDataOut : std_logic_vector(7 downto 0) := (others => '0');
     ----------------------
 
-
     -- Registered signals
     signal txDataxDP, txDataxDN : std_logic_vector(7 downto 0) := (others => '0');
     signal fifoWriteEnxDP, fifoWriteEnxDN : std_logic := '0';
+    ----------------------
 
     -- State machine
     type StateType is (IDLE_S, SEND_S, RECEIVE_S);
     signal state, rxState : StateType := IDLE_S;
+    ----------------------
 
     -- Window signals
     signal RightBoundxDP, RightBoundxDN : integer range 0 to voltageChangeRegLengthC := 0;
     signal LeftBoundxDP, LeftBoundxDN   : integer range 0 to voltageChangeRegLengthC := 7;
+    ----------------------
 
     -- Constants used to pad data when voltageChangeIntervalxDI has a width
     -- not multiple of 8
     constant paddingNumber : natural := voltageChangeRegLengthC mod 8;
     constant paddingVector : std_logic_vector(7 - paddingNumber downto 0) := (others => '0');
     constant RightBoundEdgeCase : integer := voltageChangeRegLengthC - paddingNumber;
-    
+    ----------------------
 
 begin
     -- Instantiate UartWrapper module
@@ -79,9 +87,11 @@ begin
             clk      => clk,
             rst      => rst,
 
+            -- UART signals
             rxUartxDI => rxFpgaxDI,
             txUartxDO => txFpgaxDO,
 
+            -- FIFO signals
             fifoDataInxDI  => txDataxDP,
             fifoDataOutxDO => fifoDataOut,
             fifoEmptyxDO   => fifoEmpty,
@@ -89,6 +99,47 @@ begin
             fifoReadxDI    => fifoRead,
             fifoWritexDI   => fifoWriteEnxDP
     );
+
+    --------------------
+    -- TX LOGIC
+    --------------------
+    logicP: process(clk, rst)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                state <= IDLE_S;
+            else
+                -- Registers update
+                LeftBoundxDP <= LeftBoundxDN;
+                RightBoundxDP <= RightBoundxDN;
+                txDataxDP <= txDataxDN;
+                fifoWriteEnxDP <= fifoWriteEnxDN;
+
+                -- FSM
+                case state is
+                    when IDLE_S =>
+                        -- Data ready to be sent
+                        if (voltageChangeRdyxDI = '1') then
+                            state <= SEND_S;
+                            -- Reset the window when starting a new transmission
+                            LeftBoundxDP <= 7;
+                            RightBoundxDP <= 0;
+                            -- Sample the data to send
+                            voltageChangeIntervalxDP <= voltageChangeIntervalxDI;
+                        end if;
+                        
+                    when SEND_S =>
+                        -- Check if the full data has been sent
+                        if (LeftBoundxDP = voltageChangeRegLengthC) then
+                            state <= IDLE_S;
+                        end if;
+
+                    when others =>
+                        state <= IDLE_S;
+                end case;
+            end if;
+        end if;
+    end process logicP;
 
     -- Write enable signal for the UART's TX FIFO
     fifoWriteEnxDN <= '1' when state = SEND_S and fifoFull = '0'
@@ -117,92 +168,34 @@ begin
                         else voltageChangeRegLengthC when state = SEND_S
                         else LeftBoundxDP;
 
-    logicP: process(clk, rst)
-    begin
-        if rising_edge(clk) then
-            if rst = '1' then
-                state <= IDLE_S;
-                LeftBoundxDP <= 7;
-                RightBoundxDP <= 0;
-            else
-                -- Registers update
-                LeftBoundxDP <= LeftBoundxDN;
-                RightBoundxDP <= RightBoundxDN;
-                txDataxDP <= txDataxDN;
-                fifoWriteEnxDP <= fifoWriteEnxDN;
-
-                -- FSM
-                case state is
-                    when IDLE_S =>
-                        -- Data ready to be sent
-                        if (voltageChangeRdyxDI = '1') then
-                            state <= SEND_S;
-                            -- Reset the window when starting a new transmission
-                            LeftBoundxDP <= 7;
-                            RightBoundxDP <= 0;
-                            -- Sample the data to send
-                            voltageChangeIntervalxDP <= voltageChangeIntervalxDI;
-                        end if;
-                        
-                        -- Data received -> fifo not empty
-                        -- if (fifoEmpty = '0') then
-                        --     state <= RECEIVE_S;
-                        -- end if;
-                        -- fifoRead <= '0';
-                        
-                    when SEND_S =>
-                        -- Check if the full data has been sent
-                        if (LeftBoundxDP = voltageChangeRegLengthC) then
-                            state <= IDLE_S;
-                        end if;
-
-                    -- when RECEIVE_S =>
-                    --     fifoRead <= '1';
-                    --     state <= IDLE_S;
-                    --     if (fifoDataOut = x"CA") then
-                    --         cnt := cnt + 1;
-                    --     end if;
-
-                    --     if (cnt = 200) then
-                    --         led_g <= '1';
-                    --         cnt := 0;
-                    --     end if;
-
-                    when others =>
-                        state <= IDLE_S;
-                end case;
-            end if;
-        end if;
-    end process logicP;
 
 
-
+    --------------------
+    -- RX LOGIC
+    --------------------
     rxLogicP: process(clk, rst)
         variable cnt : integer := 0;
     begin
         if rising_edge(clk) then
             if rst = '1' then
-                -- TODO
+                rxState <= IDLE_S;
             else
 
+                -- FSM
                 case rxState is
                     when IDLE_S =>
                         -- Data received -> fifo not empty
                         if (fifoEmpty = '0') then
                             rxState <= RECEIVE_S;
                         end if;
-                        fifoRead <= '0';
+
+                        led_g <= '0';
                         
                     when RECEIVE_S =>
-                        fifoRead <= '1';
                         rxState <= IDLE_S;
-                        if (fifoDataOut = x"CA") then
-                            cnt := cnt + 1;
-                        end if;
 
-                        if (cnt = 200) then
+                        if (fifoDataOut = x"CA") then
                             led_g <= '1';
-                            cnt := 0;
                         end if;
 
                     when others =>
@@ -211,61 +204,11 @@ begin
             end if;
         end if;
     end process rxLogicP;
+
+    -- Write enable signal for the UART's RX FIFO
+    -- As the FIFO is FWFT, the read signal is asserted immediately: the byte
+    -- is already available (no need to wait a clock cycle for the data to be available
+    -- after fifoRead is asserted).
+    fifoRead <= '1' when rxState = RECEIVE_S else '0';
                 
-
-
-    
 end architecture rtl;
-
-
--- process (clk)
--- begin
---     if rising_edge(clk) then
---         rx_busy_present <= i_rx_busy;
---         rx_busy_old     <= rx_busy_present;
-
---         -- Falling edge detection
---         if rx_busy_old = '1' and rx_busy_present = '0' then
---             -- New receive data is available
---             case state is
---             when idle =>
---                 if i_rx_data(7 downto 4) = x"D" then -- DAC
---                     bytes_left <= 2;
---                     state <= receive_data;
---                     -- Pick the voltage line
---                     if i_rx_data(3 downto 0) = x"0" then
---                         voltage_line <= VthA;
---                     elsif i_rx_data(3 downto 0) = x"1" then
---                         voltage_line <= VthB;
---                     elsif i_rx_data(3 downto 0) = x"2" then
---                         voltage_line <= VthC;
---                     elsif i_rx_data(3 downto 0) = x"3" then
---                         voltage_line <= VthD;
---                     elsif i_rx_data(3 downto 0) = x"4" then
---                         voltage_line <= VthE;
---                     elsif i_rx_data(3 downto 0) = x"5" then
---                         voltage_line <= VthF;
---                     elsif i_rx_data(3 downto 0) = x"6" then
---                         voltage_line <= VthG;
---                     elsif i_rx_data(3 downto 0) = x"7" then
---                         voltage_line <= VthH;
---                     end if;
---                 end if;
---             when receive_data =>
---                 case voltage_line is
---                 when VthA =>
---                     if bytes_left = 2 then
---                         VthA(7 downto 0) <= i_rx_data(7 downto 0);
---                         bytes_left <= bytes_left - 1;
---                     elsif bytes_left = 1 then
---                         VthA(11 downto 8) <= i_rx_data(3 downto 0);
---                         state <= idle;
---                     end if;
---                 end case;
---             end case;
-                    
-
---         end if;
-
---     end if;
--- end process;
