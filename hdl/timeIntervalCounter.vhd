67507b33a15f524c121528c87f9eeedfed89c08f
Clyde Laforge <clyde.laforge@cern.ch>
2024-08-27 11:07:57 +0200

    timeIntervalCounter.vhd: partial rewrite, count is returned and reset every cycle

--- hdl/timeIntervalCounter.vhd
+++ hdl/timeIntervalCounter.vhd
@@ -19,22 +19,21 @@ entity timeIntervalCounter is
         slowFactorG : integer := 2
     );
     port (
-        clk : in  std_logic; --! 100MHz ACCURATE clock
+        clk : in  std_logic; --! Clock
         rst : in  std_logic; --! Synchronous reset
 
         inxDI : in  std_logic; --! Interval signal
 
         -- Number of activation of cp1 in the last samplexDI interval
-        lastIntervalDurationxDO : out signed(countBitwidthG - 1 downto 0)
+        lastIntervalDurationxDO : out unsigned (countBitwidthG - 1 downto 0)
 
     );
 end entity timeIntervalCounter;
 
 architecture behavioral of timeIntervalCounter is
-    signal slowCounterxDP, slowCounterxDN : unsigned(slowFactorG - 1 downto 0) := (others => '0');
-    signal intervalCounterxDN, intervalCounterxDP : unsigned (countBitwidthG - 1 downto 0) := ('1', others => '0');
+    signal intervalCounterxDN, intervalCounterxDP : unsigned (countBitwidthG + slowFactorG - 1 downto 0) := (others => '0');
 
-    signal lastIntervalDurationxDN, lastIntervalDurationxDP : signed(countBitwidthG - 1 downto 0) := ('1', others => '0');
+    signal lastIntervalDurationxDN, lastIntervalDurationxDP : unsigned (countBitwidthG - 1 downto 0) := (others => '0');
 begin
 
     ------------------------------- Registers ---------------------------------
@@ -42,27 +41,19 @@ begin
     begin
         if rising_edge(clk) then
             if (rst = '1') then
-                intervalCounterxDP <= ('1', others => '0');
-                lastIntervalDurationxDP <= ('1', others => '0');
-                slowCounterxDP <= (others => '0');
+                intervalCounterxDP <= (others => '0');
+                lastIntervalDurationxDP <= (others => '0');
             else
                 intervalCounterxDP <= intervalCounterxDN;
                 lastIntervalDurationxDP <= lastIntervalDurationxDN;
-                slowCounterxDP <= slowCounterxDN;
             end if;
         end if;
     end process regP;
 
-    slowCounterxDN <= (others => '0') when inxDI = '1' else
-                      slowCounterxDP + 1;
-
     intervalCounterxDN <= (others => '0') when inxDI = '1' else
-                          ('1', others => '0') when intervalCounterxDP(intervalCounterxDP'left) = '1' else
-                          intervalCounterxDP + 1 when slowFactorG = 0 else
-                          intervalCounterxDP + 1 when slowCounterxDP = slowCounterxDP'length ** 2 - 1 else
-                          intervalCounterxDP;
+                          intervalCounterxDP + 1;
 
-    lastIntervalDurationxDN <= signed(intervalCounterxDP) when inxDI = '1' else
+    lastIntervalDurationxDN <= intervalCounterxDP(intervalCounterxDP'left downto slowFactorG) when inxDI = '1' else
                                lastIntervalDurationxDP;
 
     lastIntervalDurationxDO <= lastIntervalDurationxDP;
