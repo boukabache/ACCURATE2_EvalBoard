4117e1369f903637d85d2398ae290d9c996af367
Mattia Consani <mattia.consani@epfl.ch>
2024-10-14 16:54:50 +0200

    scpiInterface.h: update documentation, update macros values

--- main/scpiInterface.h
+++ main/scpiInterface.h
@@ -4,7 +4,6 @@
  * @author Mattia Consani
  * 
  * This file contains the SCPI parser library implementation for the project.
- * Here the command tree is defined.
  * The SCPI parser object is initialized in the main file.
 */
 
@@ -18,11 +17,11 @@ The SCPI library supports the following macros to optimize RAM usage:
 - SCPI_HASH_TYPE : Integer size used for hashes.
 */
 #define SCPI_ARRAY_SYZE 4 //Default value = 6
-#define SCPI_MAX_TOKENS 24 //Default value = 15
-#define SCPI_MAX_COMMANDS 29 //Default value = 20
+#define SCPI_MAX_TOKENS 46 //Default value = 15
+#define SCPI_MAX_COMMANDS 46 //Default value = 20
 #define SCPI_MAX_SPECIAL_COMMANDS 0 //Default value = 0
 #define SCPI_BUFFER_LENGTH 128 //Default value = 64
-#define SCPI_HASH_TYPE uint8_t //Default value = uint8_t
+#define SCPI_HASH_TYPE uint16_t //Default value = uint8_t
 
 #ifndef SCPIINTERFACE_H
 #define SCPIINTERFACE_H
@@ -88,87 +87,19 @@ CONFigure
         :RAW?
 */
 
-/*
-Largest branch needed = 3
-i.e. STATus:OPERation:ENABle or SYSTem:ERRor:NEXT?
-*/
-//This also sets the max number of parameters
-
-/*
-Valid Tokens: 
-01: STATus
-02: OPERation
-03: CONDition
-04: ENABle
-05: EVENt
-06: QUEStionable
-07: PRESet
-08: SYSTem
-09: ERRor
-10: NEXt
-11: VERSion
-12: *CLS
-13: *ESE
-14: *ESR
-15: *IDN
-16: *OPC
-17: *RST
-18: *SRE
-19: *STB
-20: *TST
-21: *WAI
-Total number of valid tokens: 21
-*/
-
-/*
-Valid Commands:
-01: STATus:OPERation:CONDition?
-02: STATus:OPERation:ENABle
-03: STATus:OPERation?
-04: STATus:OPERation:EVENt?
-05: STATus:QUEStionable:CONDition?
-06: STATus:QUEStionable:ENABle
-07: STATus:QUEStionable?
-08: STATus:QUEStionable:EVENt?
-09: STATus:PRESet
-10: SYSTem:ERRor?
-11: SYSTem:ERRor:NEXT?
-12: SYSTem:VERSion?
-13: *CLS
-14: *ESE
-15: *ESE?
-16: *ESR?
-17: *IDN?
-18: *OPC
-19: *OPC?
-20: *RST
-21: *SRE
-22: *SRE?
-23: *STB
-24: *TST?
-25: *WAI
-Total number of valid commands: 25
-*/
-
-/*
-No special commands used
-*/
-
-/*
-The message buffer should be large enough to fit all the incoming message
-For example, the multicommand message
-"*RST; *cls; status:operation:enable; status:questionable:enable;\n"
-will need at least 67 byte buffer length.
-*/
-
-/*
-If needed, to avoid hash crashes (two commands have the same hash),
-change SCPI_HASH_TYPE to uint16_t or uint32_t.
-*/
-
-
+/**
+ * @brief The SCPI parser object
+ * 
+ * The SCPI_Parser object is defined in the main file.
+ */
 extern SCPI_Parser my_instrument;
 
+/**
+ * @brief Initialize the SCPI interface
+ * 
+ * Set the parser command tree and other settings. The SCPI_Parser object is
+ * defined in the main file.
+ */
 void init_scpiInterface();
 
 
